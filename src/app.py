6f0958f6231e3434875e78ae7b6852fe18f65f14
Thiago Freitas <thiagosistemas3@gmail.com>
2021-01-24 21:19:34 -0300

    a

--- src/app.py
+++ src/app.py
@@ -6,31 +6,27 @@ from utils import Command
 command_data = Command()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 
-@app.route('/command')
+
+@app.route("/command")
 def command():
-    command = request.args.get('command')
+    command = request.args.get("command")
 
     executor = command_data.find_command(command)
-
-    print("executor")
-    print("executor")
-    print("executor")
-    print(executor)
 
     if not executor:
         abort(404, description="Command not found")
 
-
-    socketio.emit("my_message", {'command': executor})
+    socketio.emit("my_message", {"command": executor})
     return ""
 
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
 
-if __name__ == '__main__':
+@socketio.on("disconnect")
+def test_disconnect():
+    print("Client disconnected")
+
+
+if __name__ == "__main__":
     socketio.run(app)
-   
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,13 @@
 import yaml
 
-class Command:
 
+class Command:
     def __init__(self):
         self.data = yaml.load(open("./commands.yaml"))
 
-    def find_command(self, command : str):
-
-        print("command")
-        print("command")
-        print("command")
-        print(command)
+    def find_command(self, command: str):
 
         for data in self.data.get("commands"):
-
-            print(data)
 
             if command.strip() == data.get("command").get("code"):
                 return data.get("command").get("exec")
